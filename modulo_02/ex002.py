bc6e5169ac12d1483d3032a3b5c48289ae72f344
Rafael <rafaelribeirong@gmail.com>
2022-06-14 14:52:42 -0300

    maior e menor numero

---
+++ modulo_02/ex002.py
@@ -0,0 +1,8 @@
+num = int(input("Digite um número: "))
+numm = int(input("Digite outro número: "))
+if num > numm:
+    print("O primeiro valor é maior!")
+elif num < numm:
+    print("O segundo valor é maior!")
+else:
+    print("Não existe valor maior, os dois são iguais!")
