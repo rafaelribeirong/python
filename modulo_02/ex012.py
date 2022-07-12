907d9badfae612e669e6c4109ecd89f6b6c7fb47
Rafael <rafaelribeirong@gmail.com>
2022-07-12 09:58:16 -0300

    soma múltiplos de três

--- modulo_02/ex012.py
+++ modulo_02/ex012.py
@@ -2,7 +2,8 @@ soma = 0
 cont = 0
 for c in range(1, 501, 2 ):
    if c % 3 == 0:
-      soma += c
-      cont += 1
+    soma += c
+    cont += 1
 print('A soma de todos os {} valores solicitados é {}'.format(cont, soma))
+
 
