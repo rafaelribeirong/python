23098346e91d91fc7c53e6312add30313808c3a9
Rafael <rafaelribeirong@gmail.com>
2022-07-12 08:36:29 -0300

    contagem regressiva

---
+++ modulo_02/ex010.py
@@ -0,0 +1,15 @@
+from time import sleep
+for c in range(10, -1, -1):
+   print(c)
+   sleep(1)
+print('BUMM!! BUMM!! BUMMMM!!')
+sleep(2)
+
+print('FELIZ')
+sleep(1)
+
+print('ANO')
+sleep(1)
+
+print('NOVO!!!')  
+sleep(1)
