bdf0a035c8dc6fc7928b8e71ce2f23be70523297
Rafael <rafaelribeirong@gmail.com>
2022-07-13 11:26:14 -0300

    maior e menor sequência

---
+++ modulo_02/ex019.py
@@ -0,0 +1,14 @@
+maior = 0
+menor = 0
+for p in range(1,6):
+   peso = float(input('Qual é o seu peso da {}º pessoa: '.format(p)))
+   if p == 1:
+      maior = peso
+      menor = peso
+   else:
+      if peso > maior:
+         maior = peso
+      if peso < menor:
+         menor = peso
+print('O maior peso lido foi  {}Kg'.format(maior))
+print('Omenor peso lido foi o {}Kg'.format(menor))
